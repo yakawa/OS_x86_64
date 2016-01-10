231ebb91278da99b16f8ce662fc4cbedbc020c2c
KAWASAKI Yasukazu <kawasaki@dev.kawa1128.jp>
2016-01-10 22:56:27 +0900

    例外、割り込みの実装

--- src/kernel/irs.s
+++ src/kernel/irs.s
@@ -6,10 +6,137 @@ irs_ignore:
 				jmp	hlt
 				iretq
 
-GLOBAL irs_div_by_0
-EXTERN div_by_0_func
-irs_div_by_0:
-				call div_by_0_func
+GLOBAL irs_DE
+EXTERN DE_func
+irs_DE:
+				call DE_func
+				jmp hlt
+				iretq
+
+GLOBAL	irs_DB
+EXTERN	DB_func
+irs_DB:
+				call DB_func
+				jmp hlt
+				iretq
+
+GLOBAL	irs_NMI
+EXTERN	NMI_func
+irs_NMI:
+				call NMI_func
+				jmp hlt
+				iretq
+
+GLOBAL	irs_BP
+EXTERN	BP_func
+irs_BP:
+				call BP_func
+				jmp hlt
+				iretq
+
+
+GLOBAL	irs_OF
+EXTERN  OF_func
+irs_OF:
+				call OF_func
+				jmp hlt
+				iretq
+
+GLOBAL	irs_BR
+EXTERN	BR_func
+irs_BR:
+				call BR_func
+				jmp	hlt
+				iretq
+
+GLOBAL	irs_UD
+EXTERN	UD_func
+irs_UD:
+				call UD_func
+				jmp hlt
+				iretq
+
+GLOBAL	irs_NM
+EXTERN	NM_func
+irs_NM:
+				call NM_func
+				jmp hlt
+				iretq
+
+GLOBAL	irs_DF
+EXTERN	DF_func
+irs_DF:
+				call DF_func
+				jmp hlt
+				iretq
+
+GLOBAL	irs_coprocessor
+EXTERN	coprocessor_func
+irs_coprocessor:
+				call coprocessor_func
+				jmp hlt
+				iretq
+
+GLOBAL	irs_TS
+EXTERN	TS_func
+irs_TS:
+				call TS_func
+				jmp hlt
+				iretq
+
+GLOBAL	irs_NP
+EXTERN	NP_func
+irs_NP:
+				call NP_func
+				jmp hlt
+				iretq
+
+GLOBAL	irs_SS
+EXTERN	SS_func
+irs_SS:
+				call SS_func
+				jmp hlt
+				iretq
+
+GLOBAL irs_GP
+EXTERN GP_func
+irs_GP:
+				call GP_func
+				jmp hlt
+				iretq
+
+GLOBAL	irs_PF
+EXTERN	PF_func
+irs_PF:
+				call PF_func
+				jmp hlt
+				iretq
+
+GLOBAL	irs_MF
+EXTERN	MF_func
+irs_MF:
+				call MF_func
+				jmp hlt
+				iretq
+
+GLOBAL	irs_AC
+EXTERN 	AC_func
+irs_AC:
+				call AC_func
+				jmp hlt
+				iretq
+
+GLOBAL	irs_MC
+EXTERN 	MC_func
+irs_MC:
+				call MC_func
+				jmp hlt
+				iretq
+
+GLOBAL	irs_XF
+EXTERN	XF_func
+irs_XF:
+				call XF_func
 				jmp hlt
 				iretq
 
